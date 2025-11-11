9d841632185fff7f096cfbfe9db314292a83a732
AL3X-AFK <alexvvaldezv@gmail.com>
2025-11-11 13:13:42 +0100

    Update toolbar

--- app/src/main/java/com/example/nicestart/Signup.java
+++ app/src/main/java/com/example/nicestart/Signup.java
@@ -10,6 +10,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class Signup extends AppCompatActivity {
 
     @Override
@@ -17,6 +19,17 @@ public class Signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
+
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+
+        //Establece la Toolbar como ActionBar
+        setSupportActionBar(toolbar);
+
+        //Define la acción de la flecha
+        toolbar.setNavigationOnClickListener(v -> {
+            Intent intent = new Intent(Signup.this, Login.class);
+            startActivity(intent);
+        });
     }
 
     public void openMain(View v){
