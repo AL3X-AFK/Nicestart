4aa33d8a6b705ddd9c4be1f45d1ded621e0682d6
AL3X-AFK <alexvvaldezv@gmail.com>
2026-01-20 13:52:48 +0100

    new activity - bottom menu bar

---
+++ app/src/main/java/com/example/nicestart/MainBab.java
@@ -0,0 +1,64 @@
+package com.example.nicestart;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainBab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_bab);
+
+        BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar);
+        FloatingActionButton myfab = findViewById(R.id.fab);
+
+        //click event en el  FAB
+        myfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainBab.this, "FAB Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //click event en el Hamburguer menu
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(MainBab.this, "Menu clicked", Toast.LENGTH_SHORT).show();
+//                sheetBehavior = BottomSheetBehavior.from(sheet);
+                Toast.makeText(MainBab.this, "Hamburguer Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //click event en el Bottom bar menu item
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                if (item.getItemId() == R.id.heart) {
+                    Toast.makeText(MainBab.this, "Added to favourites", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.search) {
+                    Toast.makeText(MainBab.this, "Beginning search", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+
+
+
+    }
+}
